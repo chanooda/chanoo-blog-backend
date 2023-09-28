dd4e6e0e762012f2955b2078913e77a7e61138c7
차누 <chanoo@chanuui-MacBookAir.local>
2023-09-28 22:26:17 +0900

    BLOG-007 feat, 이미지 최적화하기

---
+++ src/pipe/image.pipe.ts
@@ -0,0 +1,31 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import * as sharp from 'sharp';
+import { getFileName } from 'src/utils/fileUtils';
+
+@Injectable()
+export class TransformImage implements PipeTransform {
+  maxWidth = 800;
+  maxHeight = 1000;
+  async transform(
+    value: Express.Multer.File,
+    metadata: ArgumentMetadata,
+  ): Promise<Express.Multer.File> {
+    const image = sharp(value.buffer);
+    const { width, height } = await image.metadata();
+    console.log(await image.metadata());
+    if (width > height && width > this.maxWidth) {
+      image.resize({ width: this.maxWidth, fit: 'contain' });
+    }
+    if (height > width && height > this.maxHeight) {
+      image.resize({ height: this.maxHeight, fit: 'contain' });
+    }
+    const parsedImage = await image.withMetadata().toFormat('webp').toBuffer();
+
+    return {
+      ...value,
+      buffer: parsedImage,
+      mimetype: 'image/webp',
+      originalname: `${getFileName(value.originalname)}`,
+    };
+  }
+}
