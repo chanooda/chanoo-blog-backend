136bf37f674544b9a0368985267bf2e1967e07a0
차누 <chanoo@chanuui-MacBookAir.local>
2023-10-01 04:20:40 +0900

    docs: github action 환경변수 확인

--- after-install.sh
+++ after-install.sh
@@ -1,4 +1,5 @@
 cd /home/ubuntu/blog-backend
+docker image rm blog-backend
 docker build -t blog-backend .
 docker rm --force blog-backend
 docker run -dit -p 4000:4000 --name blog-backend blog-backend
